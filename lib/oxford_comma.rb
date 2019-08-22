c6ba9631aea8e60596965445c5599b6f04e8f08e
Ashley Norton <nortonashleym@gmail.com>
2019-08-22 18:08:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,8 @@ def oxford_comma(array)
   if array.size == 2
    array.join(" and ")
   elsif array.size >= 3
-    array[0...-1].join(", ").
+    array[0...-1].join(", ") << "and "
+  else 
+    p array 
+  end
 end
